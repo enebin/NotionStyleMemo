e2c433483613efc4b9292acf8d59a7ede9868123
enebin <46271447+enebin@users.noreply.github.com>
2021-10-11 20:35:43 +0900

    Initial Commit

---
+++ NotionStyleMemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NotionStyleMemo
+//
+//  Created by 이영빈 on 2021/10/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NotionStyleMemo/NotionStyleMemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotionStyleMemoApp.swift
+//  NotionStyleMemo
+//
+//  Created by 이영빈 on 2021/10/11.
+//
+
+import SwiftUI
+
+@main
+struct NotionStyleMemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
